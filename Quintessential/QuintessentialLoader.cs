f4c2a07579d39845a33a34a3be52bbbbd9ebcb6c
andytampan <andywirawan240510@gmail.com>
2026-06-27 18:19:38 +0800

    Added "cutscene" option
    Added new type for the chapter model which is cutscene. Currently the only way to add background is via code, although it is being worked on.

--- Quintessential/QuintessentialLoader.cs
+++ Quintessential/QuintessentialLoader.cs
@@ -605,6 +605,12 @@ SomeZipIDontLike.zip");
                                 campaign.field_2309[j].field_2314.Add(cItem);
                                 break;
                             }
+                        case "cutscene":
+                            {
+                                cItem = new(entry.ID, class_134.method_253(entry.Title, string.Empty), (enum_129)1, struct_18.field_1431, requirement, class_238.field_1992.field_970, class_238.field_1991.field_1830); 
+                                break;
+                            }
+                        // case for document
                         default:
                             Logger.Log($"Campaign entry in {c.Name} has unknown type {entry.Type}, skipping");
                             continue;
